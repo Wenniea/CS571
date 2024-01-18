28424a41d3d1c32a955578ac7f416dcc7ebe17f3
Wenniea <77358097+Wenniea@users.noreply.github.com>
2024-01-18 16:29:21 -0600

    Add files via upload

--- hw6/src/structrual/BadgerApp.js
+++ hw6/src/structrual/BadgerApp.js
@@ -1,1 +1,50 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import BadgerLayout from './BadgerLayout';
+import BadgerLogin from '../auth/BadgerLogin';
+import BadgerRegister from '../auth/BadgerRegister';
+import BadgerLogout from '../auth/BadgerLogout';
+import BadgerChatroom from '../content/BadgerChatroom';
+import BadgerChatHome from '../content/BadgerChatHome';
+import BadgerNoMatch from '../content/BadgerNoMatch';
+import LoginContext from '../../contexts/LoginContext.js';
+
+
+function BadgerApp() {
+  const [chatrooms, setChatrooms] = useState([]);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    fetch('https://cs571.org/s23/hw6/api/chatroom', {
+      headers: {
+        "X-CS571-ID": "bid_2a1709990731052fcc9b",
+      }
+    }).then(res => res.json()).then(json => {
+      setChatrooms(json)
+    })
+  }, []);
+
+  return (
+    <LoginContext.Provider value={[loggedIn, setLoggedIn]}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BadgerLayout chatrooms={chatrooms} />}>
+            <Route index element={<BadgerChatHome />} />
+            <Route path="/login" element={<BadgerLogin />}></Route>
+            <Route path="/register" element={<BadgerRegister />}></Route>
+            <Route path="/logout" element={<BadgerLogout />}></Route>
+            {
+              chatrooms.map(chatroom => {
+                return <Route key={chatroom} path={`chatrooms/${chatroom}`} element={<BadgerChatroom name={chatroom} />} />
+              })
+            }
+            <Route path="*" element={<BadgerNoMatch />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </LoginContext.Provider>
+  );
+}
+
+export default BadgerApp;
