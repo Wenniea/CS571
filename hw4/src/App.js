8b6247a3b8498bf13929f8d63a339010d909b757
Wenniea <77358097+Wenniea@users.noreply.github.com>
2024-01-18 14:56:53 -0600

    upload src files

---
+++ hw4/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Classroom from './components/Classroom';
+
+function App() {
+  return <div>
+    <h1>Badger Book</h1>
+    <Classroom />
+  </div>;
+}
+
+export default App;
